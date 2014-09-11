22b74728dbb40dbdbdf63f7d50732009bc61c4df
Rich Wareham <rjw57@cam.ac.uk>
2014-09-11 18:10:16 +0100

    testmock: re-factor into test fixture class

--- test/testmock.py
+++ test/testmock.py
@@ -51,47 +51,51 @@ def wait_for_and_compress_frames(kinect, min_count, timeout):
     # Return the packets received and how long we waited
     return compressed, (end-start)
 
-def test_create_mock():
-    with mock.MockKinect() as kinect:
-        pass
+class TestMock:
+    def setup(self):
+        self.kinect = mock.MockKinect()
 
-def test_getting_frames():
-    with mock.MockKinect() as kinect:
-        count, t = wait_for_frames(kinect, 1, 0.5)
-    log.info('Got {0} frame(s) in {1:.2f} seconds'.format(count, t))
-    assert count >= 1
+    def test_create_mock(self):
+        with self.kinect as kinect:
+            pass
 
-def test_getting_enough_fps():
-    with mock.MockKinect() as kinect:
-        count, t = wait_for_frames(kinect, 30, 0.5)
-    fps = float(count) / t
-    log.info('Mock kinect gave {0} frames in {1:.2f} seconds => fps = {2:.2f}'.format(
-        count, t, fps))
-    if fps < TARGET_FPS:
-        log.error('FPS should be > {0}'.format(TARGET_FPS))
-    assert fps >= TARGET_FPS
+    def test_getting_frames(self):
+        with self.kinect as kinect:
+            count, t = wait_for_frames(kinect, 1, 0.5)
+        log.info('Got {0} frame(s) in {1:.2f} seconds'.format(count, t))
+        assert count >= 1
 
-def test_getting_compressed_frames():
-    with mock.MockKinect() as kinect:
-        packets, t = wait_for_and_compress_frames(kinect, 10, 0.5)
-    log.info('Got {0} compressed packets in {1:.2f} seconds'.format(len(packets), t))
-    assert len(packets) > 0
+    def test_getting_enough_fps(self):
+        with self.kinect as kinect:
+            count, t = wait_for_frames(kinect, 30, 0.5)
+        fps = float(count) / t
+        log.info('Mock kinect gave {0} frames in {1:.2f} seconds => fps = {2:.2f}'.format(
+            count, t, fps))
+        if fps < TARGET_FPS:
+            log.error('FPS should be > {0}'.format(TARGET_FPS))
+        assert fps >= TARGET_FPS
 
-def test_getting_good_enough_compression():
-    with mock.MockKinect() as kinect:
-        packets, t = wait_for_and_compress_frames(kinect, 1024, 2.0)
-    log.info('Got {0} compressed packets in {1:.2f} seconds'.format(len(packets), t))
-    size = sum(sum(len(x) for x in y) for y in packets)
-    data_rate = (float(size) / t) / (1024*1024)
-    log.info('Total size is {0} bytes => {1:.2f} Mbytes/s'.format(size, data_rate))
-    assert data_rate < 80 * 1024 * 1024
+    def test_getting_compressed_frames(self):
+        with self.kinect as kinect:
+            packets, t = wait_for_and_compress_frames(kinect, 10, 0.5)
+        log.info('Got {0} compressed packets in {1:.2f} seconds'.format(len(packets), t))
+        assert len(packets) > 0
 
-def test_getting_fast_enough_compression():
-    with mock.MockKinect() as kinect:
-        packets, t = wait_for_and_compress_frames(kinect, 120, 2.0)
-    fps = float(len(packets)) / t
-    log.info('Mock kinect gave {0} compressed packets in {1:.2f} seconds => fps = {2:.2f}'.format(
-        len(packets), t, fps))
-    if fps < TARGET_FPS:
-        log.error('FPS should be > {0}'.format(TARGET_FPS))
-    assert fps >= TARGET_FPS
+    def test_getting_good_enough_compression(self):
+        with self.kinect as kinect:
+            packets, t = wait_for_and_compress_frames(kinect, 1024, 2.0)
+        log.info('Got {0} compressed packets in {1:.2f} seconds'.format(len(packets), t))
+        size = sum(sum(len(x) for x in y) for y in packets)
+        data_rate = (float(size) / t) / (1024*1024)
+        log.info('Total size is {0} bytes => {1:.2f} Mbytes/s'.format(size, data_rate))
+        assert data_rate < 80 * 1024 * 1024
+
+    def test_getting_fast_enough_compression(self):
+        with self.kinect as kinect:
+            packets, t = wait_for_and_compress_frames(kinect, 120, 2.0)
+        fps = float(len(packets)) / t
+        log.info('Mock kinect gave {0} compressed packets in {1:.2f} seconds => fps = {2:.2f}'.format(
+            len(packets), t, fps))
+        if fps < TARGET_FPS:
+            log.error('FPS should be > {0}'.format(TARGET_FPS))
+        assert fps >= TARGET_FPS
