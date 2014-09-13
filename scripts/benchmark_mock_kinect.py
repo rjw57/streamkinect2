a73c234ef801b29c3cc5aeae88518c3c1abde931
Rich Wareham <rjw57@cam.ac.uk>
2014-09-13 09:50:58 +0100

    benchmark_mock_kinect: move over to using a Tornado IOLoop

--- scripts/benchmark_mock_kinect.py
+++ scripts/benchmark_mock_kinect.py
@@ -1,10 +1,18 @@
 from logging import getLogger
 import time
 
+import tornado.ioloop
+from zmq.eventloop import ioloop
+
+# Install the zmq tornado IOLoop version
+ioloop.install()
+
 from streamkinect2.mock import MockKinect
 from streamkinect2.compress import DepthFrameCompressor
 
 def benchmark_compressed(wait_time):
+    io_loop = tornado.ioloop.IOLoop.instance()
+
     print('Running compressed pipeline for {0} seconds...'.format(wait_time))
     packets = []
     with MockKinect() as kinect:
@@ -12,9 +20,12 @@ def benchmark_compressed(wait_time):
         @fc.on_compressed_frame.connect_via(fc)
         def new_compressed_frame(_, compressed_frame):
             packets.append(compressed_frame)
+
         then = time.time()
-        time.sleep(5)
+        io_loop.call_later(5, io_loop.stop)
+        io_loop.start()
         now = time.time()
+
     delta = now - then
     data_size = sum(len(p) for p in packets)
     data_rate = float(data_size) / delta # bytes/sec
@@ -23,15 +34,20 @@ def benchmark_compressed(wait_time):
     print('Data rate is {0:2f} Mbytes/second'.format(data_rate / (1024*1024)))
 
 def benchmark_mock(wait_time):
+    io_loop = tornado.ioloop.IOLoop.instance()
+
     print('Running mock kinect for {0} seconds...'.format(wait_time))
     state = { 'n_frames': 0 }
     with MockKinect() as kinect:
-        then = time.time()
         @kinect.on_depth_frame.connect_via(kinect)
         def f(kinect, depth_frame):
             state['n_frames'] += 1
-        time.sleep(wait_time)
+
+        then = time.time()
+        io_loop.call_later(5, io_loop.stop)
+        io_loop.start()
         now = time.time()
+
     delta = now - then
     fps = state['n_frames'] / delta
     print('Mock kinect runs at {0:.2f} frames/second'.format(fps))
